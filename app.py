ce52277f52fda5b9884156110a37bd68f9616404
ARYA3431 <asheeshy100@gmail.com>
2026-03-28 01:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ if st.button("Submit"):
 
     with pd.ExcelWriter(FILE_NAME, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
         df.to_excel(writer, sheet_name=current_month, index=False)
-
+    st.write("Columns:", df.columns)
     st.success("Data Saved Successfully ✅")
 
     st.subheader("Updated Data Preview")
