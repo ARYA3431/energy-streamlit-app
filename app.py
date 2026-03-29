afd3f66b13217898ac6fd73df5dfd3bb4e1df762
ARYA3431 <asheeshy100@gmail.com>
2026-03-29 23:10:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -324,10 +324,10 @@ if st.button("Submit"):
 # UPDATE ALL INPUT VALUES
 # ==============================
 
-    for group in [
+for group in [
         tr_values, lhf_values, lcss9_values, lcss8_values,
         ccm_values, fan_values, rcph_values, lcp_values, other_values
-    ]:
+]:
         for key, val in group.items():
             update_excel(ws, col_index, key, val)
 
